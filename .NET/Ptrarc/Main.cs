c3b184ddfa2154eedcae93237d55715c452821f5
星梦StarsDream <starsdream00@icloud.com>
2022-12-24 16:11:53 +0800

    更新

--- .NET/Ptrarc/Main.cs
+++ .NET/Ptrarc/Main.cs
@@ -1,25 +1,29 @@
-﻿using LLNET.Core;
-using LLNET.Event;
-using LLNET.Hook;
+﻿using LiteLoader.Hook;
+using LiteLoader.NET;
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
+namespace Ptrarc;
 [PluginMain("Ptrarc")]
-public class Ptrarc : IPluginInitializer {
+public class Ptrarc : IPluginInitializer
+{
     public string Introduction => "用于 源域服务器群 的功能插件";
     public Dictionary<string, string> MetaData => new();
     public Version Version => new(1, 0, 0);
-    public void OnInitialize() {
-        Thook.RegisterHook<Hide_Seed_Hook, Hide_Seed_HookDelegate>();
+    public void OnInitialize()
+    {
+        Thook.RegisterHook<HideSeedHook, HideSeedHookDelegate>();
     }
 }
 
-internal delegate void Hide_Seed_HookDelegate(IntPtr a1, IntPtr a2);
+internal delegate void HideSeedHookDelegate(IntPtr a1, IntPtr a2);
 [HookSymbol("?write@StartGamePacket@@UEBAXAEAVBinaryStream@@@Z")]
-internal class Hide_Seed_Hook : THookBase<Hide_Seed_HookDelegate> {
-    public override Hide_Seed_HookDelegate Hook =>
-        (IntPtr a1, IntPtr a2) => {
+internal class HideSeedHook : THookBase<HideSeedHookDelegate>
+{
+    public override HideSeedHookDelegate Hook =>
+        (a1, a2) =>
+        {
             Marshal.WriteInt64(a1, 48, 0);
             Original(a1, a2);
         };
