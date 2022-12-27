25c1586fcb84bf4b8b907adc66ec3a4dc998a41b
星梦StarsDream <starsdream00@icloud.com>
2022-12-27 18:21:36 +0800

    更新

--- Script API/scripts/AntiCreeper.js
+++ Script API/scripts/AntiCreeper.js
@@ -1,5 +1,5 @@
-import * as mc from "mojang-minecraft";
+import * as mc from "@minecraft/server";
 
-mc.world.events.beforeExplosion.subscribe((e) => {
-    if (e.source.id == "minecraft:creeper") e.impactedBlocks = [];
+mc.world.events.beforeExplosion.subscribe((arg) => {
+    if (arg.source.typeId == "minecraft:creeper") arg.impactedBlocks = [];
 });
