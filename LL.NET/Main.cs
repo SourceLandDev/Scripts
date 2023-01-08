ce0785b57a5053d4805d5e5b82a016d304d55f5f
星梦StarsDream <starsdream00@icloud.com>
2023-01-08 23:54:47 +0800

    增加AutoSupplyItem

--- LL.NET/Main.cs
+++ LL.NET/Main.cs
@@ -1,5 +1,6 @@
 ﻿using LiteLoader.Hook;
 using LiteLoader.NET;
+using MC;
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
@@ -14,6 +15,7 @@ public class Ptrarc : IPluginInitializer
     public void OnInitialize()
     {
         Thook.RegisterHook<RandomSeedHook, RandomSeedHookDelegate>();
+        Thook.RegisterHook<AutoSupplyItemHook, AutoSupplyItemHookDelegate>();
     }
 }
 
@@ -26,5 +28,34 @@ internal class RandomSeedHook : THookBase<RandomSeedHookDelegate>
         {
             Marshal.WriteInt64(a1, 48, Random.Shared.NextInt64());
             Original(a1, a2);
+        };
+}
+
+internal delegate void AutoSupplyItemHookDelegate(nint @this, nint a2, int a3, bool a4);
+[HookSymbol("?useItem@Player@@UEAAXAEAVItemStackBase@@W4ItemUseMethod@@_N@Z")]
+internal class AutoSupplyItemHook : THookBase<AutoSupplyItemHookDelegate>
+{
+    public override AutoSupplyItemHookDelegate Hook =>
+        (@this, a2, a3, a4) =>
+        {
+            Original(@this, a2, a3, a4);
+            Player player = new(@this);
+            ItemStackBase itemStackBase = new(a2);
+            if (!itemStackBase.IsNull) return;
+            Container inventory = player.Inventory;
+            ItemStack handSlot = player.HandSlot;
+            int slotIndex = inventory.FindFirstSlotForItem(handSlot);
+            for (int i = 0; i <= inventory.Size; i++)
+            {
+                ItemStack item = inventory.GetSlot(i);
+                if (item.IsNull || !itemStackBase.MatchesItem(item))
+                {
+                    continue;
+                }
+                inventory.AllSlots[slotIndex] = item.Clone();
+                inventory.AllSlots[i] = ItemStack.EMPTY_ITEM;
+                player.RefreshInventory();
+                break;
+            }
         };
 }
