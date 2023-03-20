67b02e674891b12b287c8bd231e8e352a7b32c2c
星梦喵吖 <starsdream00@icloud.com>
2023-03-20 21:43:12 +0800

    修复

--- Script API/scripts/AntiExplosion.js
+++ Script API/scripts/AntiExplosion.js
@@ -6,5 +6,5 @@ mc.world.events.beforeExplosion.subscribe((arg) => {
         arg.source.typeId == "minecraft:creeper" ||
         arg.source.typeId == "minecraft:fireball"
     )
-        arg.setImpactedBlocks({ x: 0, y: 0, z: 0 });
+        arg.setImpactedBlocks([]);
 });
