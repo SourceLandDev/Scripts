7d6d2996ee4a5f86c48b693d4d609d79b96e1e4a
星梦 <starsdream00@icloud.com>
2023-10-07 15:26:55 +0800

    重构

--- LLSE/ForSourceLand/ChainMining.js
+++ LLSE/ForSourceLand/ChainMining.js
@@ -34,340 +34,9 @@ English:
 ll.registerPlugin("ChainMining", "连锁采集", [1, 0, 0]);
 
 const config = new JsonConfigFile("plugins/ChainMining/config.json");
-const defaultState = config.init("defaultState", false);
+const alwaysEnabled = config.init("alwaysEnabled", false);
 const maxChain = config.init("maxChain", 2 ** 4);
-const blockList = config.init("blockList", {
-    "minecraft:wooden_pickaxe": {
-        undefined: {
-            "minecraft:coal_ore": -1,
-            "minecraft:quartz_ore": -1,
-            "minecraft:nether_gold_ore": -1,
-            "minecraft:deepslate_coal_ore": -1,
-        },
-        16: {
-            price: 1,
-            "minecraft:ice": -1,
-            "minecraft:blue_ice": -1,
-            "minecraft:packed_ice": -1,
-        },
-    },
-    "minecraft:stone_pickaxe": {
-        undefined: {
-            "minecraft:iron_ore": -1,
-            "minecraft:lapis_ore": -1,
-            "minecraft:coal_ore": -1,
-            "minecraft:quartz_ore": -1,
-            "minecraft:nether_gold_ore": -1,
-            "minecraft:deepslate_iron_ore": -1,
-            "minecraft:deepslate_lapis_ore": -1,
-            "minecraft:deepslate_coal_ore": -1,
-        },
-        16: {
-            price: 1,
-            "minecraft:ice": -1,
-            "minecraft:blue_ice": -1,
-            "minecraft:packed_ice": -1,
-        },
-    },
-    "minecraft:iron_pickaxe": {
-        undefined: {
-            "minecraft:iron_ore": -1,
-            "minecraft:gold_ore": -1,
-            "minecraft:diamond_ore": -1,
-            "minecraft:lapis_ore": -1,
-            "minecraft:redstone_ore": -1,
-            "minecraft:lit_redstone_ore": -1,
-            "minecraft:coal_ore": -1,
-            "minecraft:copper_ore": -1,
-            "minecraft:emerald_ore": -1,
-            "minecraft:quartz_ore": -1,
-            "minecraft:nether_gold_ore": -1,
-            "minecraft:deepslate_iron_ore": -1,
-            "minecraft:deepslate_gold_ore": -1,
-            "minecraft:deepslate_diamond_ore": -1,
-            "minecraft:deepslate_lapis_ore": -1,
-            "minecraft:deepslate_redstone_ore": -1,
-            "minecraft:lit_deepslate_redstone_ore": -1,
-            "minecraft:deepslate_emerald_ore": -1,
-            "minecraft:deepslate_coal_ore": -1,
-            "minecraft:deepslate_copper_ore": -1,
-        },
-        16: {
-            price: 1,
-            "minecraft:ice": -1,
-            "minecraft:blue_ice": -1,
-            "minecraft:packed_ice": -1,
-        },
-    },
-    "minecraft:diamond_pickaxe": {
-        undefined: {
-            "minecraft:iron_ore": -1,
-            "minecraft:gold_ore": -1,
-            "minecraft:diamond_ore": -1,
-            "minecraft:lapis_ore": -1,
-            "minecraft:redstone_ore": -1,
-            "minecraft:lit_redstone_ore": -1,
-            "minecraft:coal_ore": -1,
-            "minecraft:copper_ore": -1,
-            "minecraft:emerald_ore": -1,
-            "minecraft:quartz_ore": -1,
-            "minecraft:nether_gold_ore": -1,
-            "minecraft:ancient_debris": -1,
-            "minecraft:deepslate_iron_ore": -1,
-            "minecraft:deepslate_gold_ore": -1,
-            "minecraft:deepslate_diamond_ore": -1,
-            "minecraft:deepslate_lapis_ore": -1,
-            "minecraft:deepslate_redstone_ore": -1,
-            "minecraft:lit_deepslate_redstone_ore": -1,
-            "minecraft:deepslate_emerald_ore": -1,
-            "minecraft:deepslate_coal_ore": -1,
-            "minecraft:deepslate_copper_ore": -1,
-        },
-        16: {
-            price: 1,
-            "minecraft:ice": -1,
-            "minecraft:blue_ice": -1,
-            "minecraft:packed_ice": -1,
-        },
-    },
-    "minecraft:netherite_pickaxe": {
-        undefined: {
-            "minecraft:iron_ore": -1,
-            "minecraft:gold_ore": -1,
-            "minecraft:diamond_ore": -1,
-            "minecraft:lapis_ore": -1,
-            "minecraft:redstone_ore": -1,
-            "minecraft:lit_redstone_ore": -1,
-            "minecraft:coal_ore": -1,
-            "minecraft:copper_ore": -1,
-            "minecraft:emerald_ore": -1,
-            "minecraft:quartz_ore": -1,
-            "minecraft:nether_gold_ore": -1,
-            "minecraft:ancient_debris": -1,
-            "minecraft:deepslate_iron_ore": -1,
-            "minecraft:deepslate_gold_ore": -1,
-            "minecraft:deepslate_diamond_ore": -1,
-            "minecraft:deepslate_lapis_ore": -1,
-            "minecraft:deepslate_redstone_ore": -1,
-            "minecraft:lit_deepslate_redstone_ore": -1,
-            "minecraft:deepslate_emerald_ore": -1,
-            "minecraft:deepslate_coal_ore": -1,
-            "minecraft:deepslate_copper_ore": -1,
-        },
-        16: {
-            price: 1,
-            "minecraft:ice": -1,
-            "minecraft:blue_ice": -1,
-            "minecraft:packed_ice": -1,
-        },
-    },
-    "minecraft:golden_pickaxe": {
-        undefined: {
-            "minecraft:coal_ore": -1,
-            "minecraft:quartz_ore": -1,
-            "minecraft:nether_gold_ore": -1,
-            "minecraft:deepslate_coal_ore": -1,
-        },
-        16: {
-            price: 1,
-            "minecraft:ice": -1,
-            "minecraft:blue_ice": -1,
-            "minecraft:packed_ice": -1,
-        },
-    },
-    "minecraft:wooden_axe": {
-        undefined: {
-            "minecraft:oak_log": 0,
-            "minecraft:spruce_log": 0,
-            "minecraft:birch_log": 0,
-            "minecraft:jungle_log": 0,
-            "minecraft:acacia_log": 0,
-            "minecraft:dark_oak_log": 0,
-            "minecraft:mangrove_log": 0,
-            "minecraft:cherry_log": -1,
-            "minecraft:crimson_stem": 0,
-            "minecraft:warped_stem": 0,
-            "minecraft:pumpkin": -1,
-            "minecraft:melon_block": -1,
-            "minecraft:brown_mushroom_block": -1,
-            "minecraft:red_mushroom_block": -1,
-            "minecraft:cocoa": -1,
-        },
-    },
-    "minecraft:stone_axe": {
-        undefined: {
-            "minecraft:oak_log": 0,
-            "minecraft:spruce_log": 0,
-            "minecraft:birch_log": 0,
-            "minecraft:jungle_log": 0,
-            "minecraft:acacia_log": 0,
-            "minecraft:dark_oak_log": 0,
-            "minecraft:mangrove_log": 0,
-            "minecraft:cherry_log": -1,
-            "minecraft:crimson_stem": 0,
-            "minecraft:warped_stem": 0,
-            "minecraft:pumpkin": -1,
-            "minecraft:melon_block": -1,
-            "minecraft:brown_mushroom_block": -1,
-            "minecraft:red_mushroom_block": -1,
-            "minecraft:cocoa": -1,
-        },
-    },
-    "minecraft:iron_axe": {
-        undefined: {
-            "minecraft:oak_log": 0,
-            "minecraft:spruce_log": 0,
-            "minecraft:birch_log": 0,
-            "minecraft:jungle_log": 0,
-            "minecraft:acacia_log": 0,
-            "minecraft:dark_oak_log": 0,
-            "minecraft:mangrove_log": 0,
-            "minecraft:cherry_log": -1,
-            "minecraft:crimson_stem": 0,
-            "minecraft:warped_stem": 0,
-            "minecraft:pumpkin": -1,
-            "minecraft:melon_block": -1,
-            "minecraft:brown_mushroom_block": -1,
-            "minecraft:red_mushroom_block": -1,
-            "minecraft:cocoa": -1,
-        },
-    },
-    "minecraft:diamond_axe": {
-        undefined: {
-            "minecraft:oak_log": 0,
-            "minecraft:spruce_log": 0,
-            "minecraft:birch_log": 0,
-            "minecraft:jungle_log": 0,
-            "minecraft:acacia_log": 0,
-            "minecraft:dark_oak_log": 0,
-            "minecraft:mangrove_log": 0,
-            "minecraft:cherry_log": -1,
-            "minecraft:crimson_stem": 0,
-            "minecraft:warped_stem": 0,
-            "minecraft:pumpkin": -1,
-            "minecraft:melon_block": -1,
-            "minecraft:brown_mushroom_block": -1,
-            "minecraft:red_mushroom_block": -1,
-            "minecraft:cocoa": -1,
-        },
-    },
-    "minecraft:netherite_axe": {
-        undefined: {
-            "minecraft:oak_log": 0,
-            "minecraft:spruce_log": 0,
-            "minecraft:birch_log": 0,
-            "minecraft:jungle_log": 0,
-            "minecraft:acacia_log": 0,
-            "minecraft:dark_oak_log": 0,
-            "minecraft:mangrove_log": 0,
-            "minecraft:cherry_log": -1,
-            "minecraft:crimson_stem": 0,
-            "minecraft:warped_stem": 0,
-            "minecraft:pumpkin": -1,
-            "minecraft:melon_block": -1,
-            "minecraft:brown_mushroom_block": -1,
-            "minecraft:red_mushroom_block": -1,
-            "minecraft:cocoa": -1,
-        },
-    },
-    "minecraft:golden_axe": {
-        undefined: {
-            "minecraft:oak_log": 0,
-            "minecraft:spruce_log": 0,
-            "minecraft:birch_log": 0,
-            "minecraft:jungle_log": 0,
-            "minecraft:acacia_log": 0,
-            "minecraft:dark_oak_log": 0,
-            "minecraft:mangrove_log": 0,
-            "minecraft:cherry_log": -1,
-            "minecraft:crimson_stem": 0,
-            "minecraft:warped_stem": 0,
-            "minecraft:pumpkin": -1,
-            "minecraft:melon_block": -1,
-            "minecraft:brown_mushroom_block": -1,
-            "minecraft:red_mushroom_block": -1,
-            "minecraft:cocoa": -1,
-        },
-    },
-    "minecraft:wooden_hoe": {
-        undefined: {
-            "minecraft:sculk": -1,
-            "minecraft:sponge": -1,
-            "minecraft:wheat": 7,
-            "minecraft:potatoes": 7,
-            "minecraft:carrot": 7,
-            "minecraft:beetroot": 7,
-            "minecraft:brown_mushroom": -1,
-            "minecraft:red_mushroom": -1,
-            "minecraft:nether_wart": 3,
-        },
-    },
-    "minecraft:stone_hoe": {
-        undefined: {
-            "minecraft:sculk": -1,
-            "minecraft:sponge": -1,
-            "minecraft:wheat": 7,
-            "minecraft:potatoes": 7,
-            "minecraft:carrot": 7,
-            "minecraft:beetroot": 7,
-            "minecraft:brown_mushroom": -1,
-            "minecraft:red_mushroom": -1,
-            "minecraft:nether_wart": 3,
-        },
-    },
-    "minecraft:iron_hoe": {
-        undefined: {
-            "minecraft:sculk": -1,
-            "minecraft:sponge": -1,
-            "minecraft:wheat": 7,
-            "minecraft:potatoes": 7,
-            "minecraft:carrot": 7,
-            "minecraft:beetroot": 7,
-            "minecraft:brown_mushroom": -1,
-            "minecraft:red_mushroom": -1,
-            "minecraft:nether_wart": 3,
-        },
-    },
-    "minecraft:diamond_hoe": {
-        undefined: {
-            "minecraft:sculk": -1,
-            "minecraft:sponge": -1,
-            "minecraft:wheat": 7,
-            "minecraft:potatoes": 7,
-            "minecraft:carrot": 7,
-            "minecraft:beetroot": 7,
-            "minecraft:brown_mushroom": -1,
-            "minecraft:red_mushroom": -1,
-            "minecraft:nether_wart": 3,
-        },
-    },
-    "minecraft:golden_hoe": {
-        undefined: {
-            "minecraft:sculk": -1,
-            "minecraft:sponge": -1,
-            "minecraft:wheat": 7,
-            "minecraft:potatoes": 7,
-            "minecraft:carrot": 7,
-            "minecraft:beetroot": 7,
-            "minecraft:brown_mushroom": -1,
-            "minecraft:red_mushroom": -1,
-            "minecraft:nether_wart": 3,
-        },
-    },
-    "minecraft:netherite_hoe": {
-        undefined: {
-            "minecraft:sculk": -1,
-            "minecraft:sponge": -1,
-            "minecraft:wheat": 7,
-            "minecraft:potatoes": 7,
-            "minecraft:carrot": 7,
-            "minecraft:beetroot": 7,
-            "minecraft:brown_mushroom": -1,
-            "minecraft:red_mushroom": -1,
-            "minecraft:nether_wart": 3,
-        },
-    },
-});
+const blockList = config.init("blockList", {});
 const currencyType = config.init("currencyType", "llmoney");
 const currencyName = config.init("currencyName", "元");
 const eco = (() => {
@@ -397,76 +66,72 @@ const eco = (() => {
     }
 })();
 const command = config.init("command", "chainmining");
-const paidCommand = config.init("paidCommand", "paidchainmining");
 config.close();
+if (!alwaysEnabled) {
+    const cmd = mc.newCommand(command, "修改连锁采集状态。", PermType.Any);
+    cmd.overload();
+    cmd.setCallback((_cmd, ori, out, _res) => {
+        if (!ori.player) return out.error("commands.generic.noTargetMatch");
+        out.success(
+            `连锁采集已${
+                (states[ori.player.xuid] = states[ori.player.xuid]
+                    ? false
+                    : true)
+                    ? "启用"
+                    : "禁用"
+            }`
+        );
+    });
+    cmd.setup();
+}
 const states = {};
-const paidStates = {};
 const destroyingBlocks = {};
-const cmd = mc.newCommand(command, "修改连锁采集状态。", PermType.Any);
-cmd.overload();
-cmd.setCallback((_cmd, ori, out, _res) => {
-    if (!ori.player) return out.error("commands.generic.noTargetMatch");
-    out.success(
-        `连锁采集已${
-            (states[ori.player.xuid] =
-                states[ori.player.xuid] ?? defaultState ? false : true)
-                ? "启用"
-                : "禁用"
-        }`
-    );
-});
-cmd.setup();
-const paidCmd = mc.newCommand(
-    paidCommand,
-    "修改付费连锁采集状态。",
-    PermType.Any
-);
-paidCmd.overload();
-paidCmd.setCallback((_cmd, ori, out, _res) => {
-    if (!ori.player) return out.error("commands.generic.noTargetMatch");
-    out.success(
-        `付费连锁采集已${
-            (paidStates[ori.player.xuid] = paidStates[ori.player.xuid]
-                ? false
-                : true)
-                ? "启用"
-                : "禁用"
-        }`
-    );
-});
-paidCmd.setup();
 mc.listen("onDestroyBlock", (pl, bl) => {
+    if (!alwaysEnabled && !states[pl.xuid]) return;
     const it = pl.getHand();
     const effectBlocks = it.isNull()
         ? blockList.empty
         : it.type in blockList
         ? blockList[it.type]
         : blockList.undefined;
-    if (!states[pl.xuid] || !effectBlocks) return;
-    let available =
-        "undefined" in effectBlocks
-            ? bl.type in effectBlocks.undefined &&
-              (effectBlocks.undefined[bl.type] < 0 ||
-                  bl.tileData == effectBlocks.undefined[bl.type])
-            : false;
-    let price =
-        "undefined" in effectBlocks ? effectBlocks.undefined.price ?? 0 : 0;
-    const tag = it.getNbt().getTag("tag");
-    const ench = tag ? tag.getData("ench") : undefined;
-    if (!available && ench)
-        for (const e of ench.toArray()) {
+    if (!effectBlocks) return;
+    let regex = undefined;
+    let checkTileData = undefined;
+    if ("undefined" in effectBlocks)
+        for (const rule of Object.keys(effectBlocks.undefined)) {
+            const reg = new RegExp(rule);
             if (
-                !(e.id in effectBlocks) ||
-                !(bl.type in effectBlocks[e.id]) ||
-                (effectBlocks[e.id][bl.type] >= 0 &&
-                    bl.tileData != effectBlocks[e.id][bl.type])
+                !reg.test(bl.type) ||
+                ((checkTileData = effectBlocks[e.id][rule] >= 0) &&
+                    bl.tileData != effectBlocks.undefined[rule])
             )
                 continue;
-            available = true;
-            price = effectBlocks[e.id].price ?? 0;
+            regex = reg;
+            break;
         }
-    if (!available || (paidStates[pl.xuid] && price > 0 && eco.get(pl) < price))
-        return;
+    let price =
+        "undefined" in effectBlocks ? effectBlocks.undefined.price ?? 0 : 0;
+    if (!regex) {
+        const tag = it.getNbt().getTag("tag");
+        const ench = tag ? tag.getData("ench") : undefined;
+        if (ench)
+            for (const e of ench.toArray())
+                if (e.id in effectBlocks)
+                    for (const rule of Object.keys(effectBlocks[e.id])) {
+                        const reg = new RegExp(rule);
+                        if (
+                            !reg.test(bl.type) ||
+                            ((checkTileData = effectBlocks[e.id][rule] >= 0) &&
+                                bl.tileData != effectBlocks[e.id][rule])
+                        )
+                            continue;
+                        regex = reg;
+                        checkTileData = effectBlocks[e.id][rule];
+                        price = effectBlocks[e.id].price ?? 0;
+                        break;
+                    }
+    }
+    if (!regex || (price > 0 && eco.get(pl) < price)) return;
     if (!(pl.xuid in destroyingBlocks)) destroyingBlocks[pl.xuid] = [];
     destroyingBlocks[pl.xuid].push(bl.pos.toString());
     const playerPointer = pl.asPointer();
@@ -490,8 +155,8 @@ mc.listen("onDestroyBlock", (pl, bl) => {
         if (
             !(pl.xuid in destroyingBlocks) ||
             destroyingBlocks[pl.xuid].includes(nextBlock.pos.toString()) ||
-            nextBlock.type != bl.type ||
-            nextBlock.tileData != bl.tileData ||
+            !reg.test(nextBlock.type) ||
+            (checkTileData && nextBlock.tileData != bl.tileData) ||
             !NativeFunction.fromSymbol(
                 "?canDestroy@Player@@QEBA_NAEBVBlock@@@Z"
             ).call(playerPointer, nextBlockPointer)
