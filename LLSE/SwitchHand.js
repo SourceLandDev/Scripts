262d1506c717918a35659b32c9e3814e43adf3fd
星梦 <starsdream00@icloud.com>
2023-10-13 12:26:45 +0800

    初始化

---
+++ LLSE/SwitchHand.js
@@ -0,0 +1,51 @@
+/*
+English:
+    SwitchHand
+    Copyright (C) 2023  Hosiyume starsdream00@icloud.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+中文：
+    交换主副手物品
+    版权所有 © 2023  星梦 starsdream00@icloud.com
+    本程序是自由软件：你可以根据自由软件基金会发布的GNU Affero通用公共许可证的条款，即许可证的第3版，
+    或（您选择的）任何后来的版本重新发布和/或修改它。
+
+    本程序的分发是希望它能发挥价值，但没有做任何保证；甚至没有隐含的适销对路或适合某一特定目的的保证。
+    更多细节请参见GNU Affero通用公共许可证。
+
+    您应该已经收到了一份GNU Affero通用公共许可证的副本。如果没有，
+    请参阅<https://www.gnu.org/licenses/>（<https://www.gnu.org/licenses/agpl-3.0.html>）
+    及其非官方中文翻译<https://www.chinasona.org/gnu/agpl-3.0-cn.html>。
+*/
+
+"use strict";
+ll.registerPlugin("SwitchHand", "交换主副手物品", [1, 0, 0]);
+
+const config = new JsonConfigFile("plugins/SwitchHand/config.json");
+const command = config.init("command", "switchhand");
+config.close();
+const cmd = mc.newCommand(command, "交换主副手物品。", PermType.Any);
+cmd.overload();
+cmd.setCallback((_cmd, ori, out, _res) => {
+    if (!ori.player) return out.error("commands.generic.noTargetMatch");
+    const hand = ori.player.getHand();
+    const mainItem = hand.clone();
+    const offhand = ori.player.getOffHand();
+    const subItem = offhand.clone();
+    hand.set(subItem);
+    offhand.set(mainItem);
+    ori.player.refreshItems();
+});
+cmd.setup();
